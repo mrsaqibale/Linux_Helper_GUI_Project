6be2253b90eb8db7f260da098e6919f241e11814
codewithprofessor <kalikali72547254@gmail.com>
2024-07-26 21:53:10 +0500

    Initial commit

---
+++ src/main/java/com/linuxhelper/LinuxHelper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.linuxhelper;
+
+/**
+ *
+ * @author professor_ali
+ */
+public class LinuxHelper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
